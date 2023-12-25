9914d7d4db291fe26ce747197de36196ede83329
WaelAmri91 <wael.amri1@esprit.tn>
2023-12-25 21:58:39 +0100

    update environnement

--- src/app/services/employe.service.ts
+++ src/app/services/employe.service.ts
@@ -1,15 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeService {
-
+  private readonly baseUrl:String=environment.url+"api/employe/"
   constructor(private http: HttpClient) { }
   getEmployes() {
-    return this.http.get(`http://localhost:8087/api/employe/GetEmployes`);
+    //return this.http.get(`http://localhost:8087/api/employe/GetEmployes`);
+    return this.http.get(`${this.baseUrl}GetEmployes`)
   }
   addEmploye(employeData: any): Observable<any> {
     return this.http.post(
--- src/app/services/player.service.ts
+++ src/app/services/player.service.ts
@@ -1,16 +1,18 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlayerService {
-
+  private readonly baseUrl:String=environment.url+"api/player/"
   constructor(private http: HttpClient) { }
 
   getPlayers() {
-    return this.http.get(`http://localhost:8087/api/player/GetPlayers`);
+   // return this.http.get(`http://localhost:8087/api/player/GetPlayers`);
+    return this.http.get(`${this.baseUrl}GetPlayers`)
   }
 
   addPlayer(playerData: any): Observable<any> {
